6456a66929019da19833d4fb4357b902d47b5d19
Gramirez21 <r.dannysixty@gmail.com>
2024-04-12 09:14:53 -0700

    exercise 1

--- LINQ/Program.cs
+++ LINQ/Program.cs
@@ -4,7 +4,16 @@
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello, World!");
+            List <string> cardGames = new List <string> ();
+
+            cardGames.Add("Poker");
+            cardGames.Add("31");
+            cardGames.Add("21");
+            cardGames.Add("Texas hold em");
+
+            cardGames.OrderBy(x => x.Length).ToList ().ForEach(x => Console.WriteLine (x));
+            
+
         }
     }
 }
